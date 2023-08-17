a0d2ae450553a11059a57324be37f5dbf68df46f
Hudson Ventura <hudson.ventura@amaggi.com.br>
2023-08-17 08:18:10 -0400

    Organizando o arquivo principal dos exemplos.

--- src/Examples/Program.cs
+++ src/Examples/Program.cs
@@ -1,86 +1,39 @@
-﻿using Services;
+﻿using Examples;
+using Services;
 using SoftExpert.Workflow;
 
-var appsettings = Appsettings.GetSettings();
+//TODO: dependendo dos caracteres do WorkflowTitle, a instancia não pode ser criada.
+//TODO: anexar arquivo no anexo do lado esquerdo: CCF202323106
+//TODO: anexar arquivo no form
 
+//Carregar as consigurações
+var appsettings = Appsettings.GetSettings();
 string url = appsettings["url"].ToString();
 string authorization = appsettings["authorization"].ToString();
 
 
+//Criar instancia da API para utilizar na injeção de dependecias. Necessário informar a URL completa do SE e o header Authorization ou todos os headers.
 SoftExpertWorkflowApi wfAPI = new SoftExpertWorkflowApi(url, authorization);
 
 
-//CRIAR INSTANCIA DE PROCESSO
-string ProcessID = "CCF";                       //identificador do processo
-string WorkflowTitle = "Teste de integração"; ; //titulo da instancia a ser criado
-string UserID = "00000000000";                  //matricula do usuario
 
-newWorkflowResponse responseNewWF;
-try
-{
-    responseNewWF = wfAPI.newWorkflow(ProcessID, WorkflowTitle, UserID);
-}
-catch (Exception erro)
-{
-    Console.WriteLine($"Não foi possivel criar o workflow. Erro: {erro.Message}");
-    return;
-}
-string WorkflowID = responseNewWF.RecordID;
-int codigoNewWorkFlow = responseNewWF.Code;
-SoftExpert.SoftExpertResponse.STATUS sucessoNewWorkFlow = responseNewWF.Status;
-string detalhesNewWorkflow = responseNewWF.Detail;
+
+//Criar instancia de processo - Veja o exemplo complexto no arquivo newWorkflowExample.cs
+newWorkflowExample newWorkflowExample = new newWorkflowExample(wfAPI);
+newWorkflowExample.Main();
 
 
 
 
 
+//Editar um formulário
+editEntityRecordExample editEntityRecordExample = new editEntityRecordExample(wfAPI);
+editEntityRecordExample.Main();
 
 
-//EDITAR FORMULÁRIO
-Dictionary<string, string> formulario = new Dictionary<string, string>();
-formulario.Add("possuiendereco", "1"); //id do campo do formulário e valor (em string)
-formulario.Add("ramal", "N/A");
-
-Dictionary<string, Dictionary<string, string>> relacionamentos = new Dictionary<string, Dictionary<string, string>>();
-relacionamentos.Add("tipocliente", //idrelacionamento
-    new Dictionary<string, string>() {
-            //{ "campodoformdorelacionamento", "valor" },
-            { "tipo", "PESSOA JURIDICA (CNPJ)" },
-    }
-);
-
-string EntityID = "SOLCLIENTEFORNE";
-
-editEntityRecordResponse entityResponse;
-try
-{
-    entityResponse = wfAPI.editEntityRecord(WorkflowID, EntityID, formulario, relacionamentos);
-}
-catch (Exception erro)
-{
-    Console.WriteLine($"Não foi possivel editar o formulário. Erro: {erro.Message}");
-    return;
-}
-int sucessoEntity = entityResponse.Code;
-string detalhesEntity = entityResponse.Detail;
+//Executar uma atividade
+executeActivityExample executeActivityExample = new executeActivityExample(wfAPI);
+executeActivityExample.Main();
 
 
 
-
-
-//EXECUÇÃO DE ATIVIDADE
-string ActivityID = "ATIV-SOLCCF";      //ID da atividade do fluxograma
-int ActionSequence = 3;                 //Sequence da ação da atividade. Veja na lista de ações da atividade
-
-executeActivityResponse executeResponse;
-try
-{
-    executeResponse = wfAPI.executeActivity(WorkflowID, ActivityID, ActionSequence, UserID);
-}
-catch (Exception erro)
-{
-    Console.WriteLine($"Não foi possivel executar a atividade. Erro: {erro.Message}");
-    return;
-}
-var houveSucesso = executeResponse.Code;
-var detalhes = executeResponse.Detail;
